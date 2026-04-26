f4a3b04def16c140821169339a59325904bf5a50
Pistonight <pistonknight@outlook.com>
2026-04-25 19:18:23 -0700

    apply build

--- packages/monaco-editor-patched/patches/toml/toml.contribution.js
+++ packages/monaco-editor-patched/patches/toml/toml.contribution.js
@@ -1,9 +1,9 @@
-import { registerLanguage } from '../_.contribution.js';
+import { registerLanguage } from "../_.contribution.js";
 
 registerLanguage({
-	id: 'toml',
-	extensions: ['.toml'],
-	aliases: ['TOML', 'toml'],
-	mimetypes: ['application/toml', 'text/toml'],
-	loader: () => import('./toml')
+    id: "toml",
+    extensions: [".toml"],
+    aliases: ["TOML", "toml"],
+    mimetypes: ["application/toml", "text/toml"],
+    loader: () => import("./toml"),
 });
--- packages/monaco-editor-patched/src/language-ex/typescript.ts
+++ packages/monaco-editor-patched/src/language-ex/typescript.ts
@@ -1,4 +1,3 @@
 import "#monaco/vs/basic-languages/typescript/typescript.contribution.js";
 import "#monaco/vs/basic-languages/javascript/javascript.contribution.js";
 export * from "#monaco/vs/language/typescript/monaco.contribution.js";
-
